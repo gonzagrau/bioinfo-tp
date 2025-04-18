37bc57eb74a151f4fcd3e40d2adaa16e03b48cfb
lneira19 <lneira@itba.edu.ar>
2025-04-18 17:12:34 -0300

    Translation with ORFs, not completed

--- src/read_seq.py
+++ src/read_seq.py
@@ -1,11 +1,34 @@
 from Bio import GenBank, SeqIO
 import numpy as np
+import re
 
+codon_start = "ATG"
 
 def main():
     with open("../data/lep-sequence.gb") as handle:
         record = SeqIO.read(handle, format="gb")
-        print(record.seq)
+        adn_seq = record.seq
+    
+    # print(f"{adn_seq}\n\n")
+
+    frames = [adn_seq[i:] for i in range(3)] + [adn_seq.reverse_complement()[i:] for i in range(3)]
+ 
+    for i, frame in enumerate(frames):
+        print(f"Frame {i+1}: {frame}\n\n")
+        
+        match = re.search(codon_start, str(frame))
+        
+        if not match:
+            print(f"No start codon found in frame {i+1}.")
+            continue
+        
+        cds_seg = frame[match.start():]
+
+        translation = cds_seg.translate()
+        print(f"Translation {i+1}: {translation}\n\n")
+    
+
+
 
 
 if __name__ == "__main__":
