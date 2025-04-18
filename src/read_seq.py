d9203a251eef72d94a88ef131d9c0487caba8800
Gonzalo Grau <ggrau@itba.edu.ar>
2025-04-18 17:29:40 -0300

    regex to find translateble regions

--- src/read_seq.py
+++ src/read_seq.py
@@ -1,34 +1,26 @@
 from Bio import GenBank, SeqIO
+from Bio.Seq import translate
 import numpy as np
 import re
 
-codon_start = "ATG"
+cds_pattern = r"ATG(?:...)*?(?:ATT|ATC|ACT)"
+
 
 def main():
     with open("../data/lep-sequence.gb") as handle:
         record = SeqIO.read(handle, format="gb")
         adn_seq = record.seq
-    
-    # print(f"{adn_seq}\n\n")
 
-    frames = [adn_seq[i:] for i in range(3)] + [adn_seq.reverse_complement()[i:] for i in range(3)]
- 
+    frames = [adn_seq[i:] for i in range(3)] + [
+        adn_seq.reverse_complement()[i:] for i in range(3)
+    ]
+
     for i, frame in enumerate(frames):
         print(f"Frame {i+1}: {frame}\n\n")
-        
-        match = re.search(codon_start, str(frame))
-        
-        if not match:
-            print(f"No start codon found in frame {i+1}.")
-            continue
-        
-        cds_seg = frame[match.start():]
-
-        translation = cds_seg.translate()
-        print(f"Translation {i+1}: {translation}\n\n")
-    
-
-
+        cds_list = re.findall(cds_pattern, str(frame))
+        for j, cds_seq in enumerate(cds_list):
+            translation = translate(cds_seq)
+            print(f"Translation {i+1, j}: {translation}\n\n")
 
 
 if __name__ == "__main__":
